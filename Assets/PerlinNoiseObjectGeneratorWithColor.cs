de573f5d5d6150789e43ff7efccf057540830431
umeda <umeda@team-lab.com>
2017-04-06 17:54:24 +0900

    色分けする機能を持った PerlinNoiseObjectGeneratorWithColor を追加。

--- Assets/PerlinNoiseObjectGeneratorWithColor.cs
+++ Assets/PerlinNoiseObjectGeneratorWithColor.cs
@@ -21,7 +21,12 @@ public class PerlinNoiseObjectGeneratorWithColor : PerlinNoiseObjectGenerator
     #endregion Struct
 
     #region Field
-    
+
+    /// <summary>
+    /// オブジェクトに設定される色。
+    /// </summary>
+    public Color[] objectColors;
+
     /// <summary>
     /// 分布のシードの数。
     /// </summary>
@@ -33,7 +38,7 @@ public class PerlinNoiseObjectGeneratorWithColor : PerlinNoiseObjectGenerator
     public float nearDistributionThreshold = 10;
 
     /// <summary>
-    /// テクスチャインデックスの分布を示すデータのリスト。
+    /// 色の分布を示すデータのリスト。
     /// </summary>
     protected List<ColorDistributionData> colorDistributionDataList = new List<ColorDistributionData>();
 
@@ -46,58 +51,58 @@ public class PerlinNoiseObjectGeneratorWithColor : PerlinNoiseObjectGenerator
     /// </summary>
     protected virtual void Start()
     {
-
+        // シード値を生成します。
+        GenerateColorDistributionDataSeed();
     }
 
     /// <summary>
-    /// 更新時に呼び出されます。
+    /// 色の分布のシード値を生成します。
     /// </summary>
-    protected virtual void Update()
+    protected virtual void GenerateColorDistributionDataSeed()
     {
-        if (this.objectCountMax <= this.objectList.Count)
+        // (1) 最初のデータを追加します。
+
+        this.colorDistributionDataList.Add(new ColorDistributionData()
         {
-            GameObject.Destroy(this.objectList[0]);
-            this.objectList.RemoveAt(0);
+            color = this.objectColors[Random.Range(0, this.objectColors.Length)]
+        });
+
+        // (2) ある程度の分布データが溜まるまで、ランダムな位置をサンプリングして分布データを更新します。
+
+        int forceBreakCount = 10000;
+        int forceBreakCounter = 0;
+
+        while (true)
+        {
+            Vector3 randomPosition = new Vector3()
+            {
+                x = Random.value,
+                y = Random.value,
+                z = 0
+            };
+
+            UpdateColorDistributionData(randomPosition);
+
+            if (this.distributionSeedCount <= this.colorDistributionDataList.Count)
+            {
+                break;
+            }
+
+            forceBreakCounter += 1;
+
+            if (forceBreakCount <= forceBreakCounter)
+            {
+                break;
+            }
         }
 
-        if (this.removeAllObject)
-        {
-            RemoveAllObject();
-            this.removeAllObject = false;
-        }
-
-        //GenerateObjectRandom();
-        GenerateObjectPerlinNoise();
+        Debug.Log(this.GetType() + " : Color DistributionData Seed Count : " + this.colorDistributionDataList.Count);
     }
 
     /// <summary>
     /// オブジェクトを生成します。
     /// </summary>
-    protected virtual void GenerateObjectRandom()
-    {
-        Camera camera      = Camera.main;
-        float randomValueX = Random.value;
-        float randomValueY = Random.value;
-
-        Vector3 randomPoint  = new Vector3(randomValueX * camera.pixelWidth,
-                                           randomValueY * camera.pixelHeight,
-                                           0);
-        Vector3 worldPoint   = Camera.main.ScreenToWorldPoint(randomPoint);
-                worldPoint.z = camera.transform.position.z + (camera.farClipPlane - camera.nearClipPlane) / 2;
-
-        GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-
-        gameObject.transform.position = worldPoint;
-        gameObject.transform.localScale *= this.objectScale;
-        gameObject.transform.SetParent(base.gameObject.transform);
-
-        this.objectList.Add(gameObject);
-    }
-
-    /// <summary>
-    /// オブジェクトを生成します。
-    /// </summary>
-    protected virtual void GenerateObjectPerlinNoise()
+    protected override void GenerateObjectPerlinNoise()
     {
         float randomValueX = Random.value;
         float randomValueY = Random.value;
@@ -108,6 +113,8 @@ public class PerlinNoiseObjectGeneratorWithColor : PerlinNoiseObjectGenerator
         {
             return;
         }
+
+        // (1) オブジェクトを生成して追加します。
 
         Camera camera = Camera.main;
 
@@ -124,230 +131,99 @@ public class PerlinNoiseObjectGeneratorWithColor : PerlinNoiseObjectGenerator
         gameObject.transform.SetParent(base.gameObject.transform);
 
         this.objectList.Add(gameObject);
+
+
+        // (2) オブジェクトを追加する座標をもとに、分布データを更新して、このオブジェクトの色を決定します。
+        //     ここではスクリーン座標系で算出している点に注意してください。
+
+        Vector3 randomPosition = new Vector3()
+        {
+            x = randomValueX,
+            y = randomValueY,
+            z = 0
+        };
+
+        Color objectColor = UpdateColorDistributionData(randomPosition);
+
+        SetColorToObject(gameObject, objectColor);
+    }
+
+    /// <summary>
+    /// 指定した座標を使って色の分布のデータを更新し、その座標の色を取得します。
+    /// </summary>
+    /// <param name="position">
+    /// 更新に利用する座標。
+    /// </param>
+    /// <returns>
+    /// 指定した座標に分布する色。
+    /// </returns>
+    protected virtual Color UpdateColorDistributionData(Vector3 position)
+    {
+        float minlength = Vector3.SqrMagnitude(position - this.colorDistributionDataList[0].position);
+        int   minlengthindex = 0;
+
+        // (1) 既存の分布データの中から近い距離にある分布データを探します。
+
+        for (int i = 1; i < this.colorDistributionDataList.Count; i++)
+        {
+            float length = Vector3.SqrMagnitude(position - this.colorDistributionDataList[i].position);
+
+            if (length < minlength)
+            {
+                minlength = length;
+                minlengthindex = i;
+            }
+        }
+
+        // (2) 既存の最寄りの分布データと距離が離れていたら新しい分布データとして採用します。
+
+        ColorDistributionData minLengthColorDistributionData = this.colorDistributionDataList[minlengthindex];
+        Color objectColor;
+
+        if (this.nearDistributionThreshold < minlength)
+        {
+            objectColor = this.objectColors[Random.Range(0, this.objectColors.Length)];
+
+            this.colorDistributionDataList.Add(new ColorDistributionData()
+            {
+                color = objectColor,
+                position = position
+            });
+        }
+
+        // (3) 既存の分布データと距離が近ければ中間の位置を分布データとして書き換えます。
+
+        else
+        {
+            objectColor = minLengthColorDistributionData.color;
+
+            this.colorDistributionDataList[minlengthindex] = new ColorDistributionData()
+            {
+                color = objectColor,
+                position = (minLengthColorDistributionData.position + position) / 2,
+            };
+        }
+
+        return objectColor;
+    }
+
+    /// <summary>
+    /// GameObject に色を付けます。
+    /// </summary>
+    /// <param name="gameObject">
+    /// 色を付ける GameObject.
+    /// </param>
+    /// <param name="color">
+    /// GameObject に付ける色。
+    /// </param>
+    protected virtual void SetColorToObject(GameObject gameObject, Color color)
+    {
+        MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
+
+        materialPropertyBlock.SetColor("_Color", color);
+
+        gameObject.GetComponent<Renderer>().SetPropertyBlock(materialPropertyBlock);
     }
 
     #endregion Method
 }
-
-//using UnityEngine;
-//using System.Collections.Generic;
-
-//namespace Sagaya.Effects.FlowerTable
-//{
-//    /// <summary>
-//    /// テーブルの上に皿が置かれたとき、花が咲くエフェクト(グローバル)。
-//    /// </summary>
-//    [System.Serializable]
-//    public class Effect_FlowerTableGlobal_Tsutuji : Effect_FlowerTableGlobal
-//    {
-//        #region Struct
-
-//        /// <summary>
-//        /// テクスチャインデックスの分布を示すデータ。
-//        /// </summary>
-//        protected struct TextureIndexDistributionData
-//        {
-//            public int textureIndex;
-//            public Vector3 position;
-//        }
-
-//        #endregion Struct
-
-//        #region Field
-
-//        /// <summary>
-//        /// 分布のシードの数。
-//        /// </summary>
-//        public int distributionSeedCount = 8;
-
-//        /// <summary>
-//        /// 分布が近いと判定する距離。
-//        /// </summary>
-//        public float nearDistributionThreshold = 10;
-
-//        /// <summary>
-//        /// テクスチャインデックスの分布を示すデータのリスト。
-//        /// </summary>
-//        protected List<TextureIndexDistributionData> textureIndexDistributionDataList = new List<TextureIndexDistributionData>();
-
-//        #endregion Field
-
-//        #region Method
-
-//        #region EffectBaseStateMachine
-
-//        /// <summary>
-//        /// StateEnterCallback メソッドから呼び出され実行される処理です。
-//        /// </summary>
-//        protected override void StateEnterCallbackFunction()
-//        {
-//            switch (GetStateID())
-//            {
-//                case State.c_EffectInit:
-//                    {
-//                        this.flowerTableEffectManager = FlowerTableEffectManager.Instance;
-//                        GenerateTextureIndexDistributionDataSeed();
-//                        break;
-//                    }
-//                case State.f_OutroAnim:
-//                    {
-//                        // すべての花を散らせます。
-//                        this.flowerManager.SetFlowersToDie(this.maxWaitTimeSecWhenFlowerDieInOutro);
-//                        break;
-//                    }
-//            }
-//        }
-
-//        #endregion EffectBaseStateMachine
-
-//        /// <summary>
-//        /// テクスチャの分布データのシード値を生成します。
-//        /// </summary>
-//        protected virtual void GenerateTextureIndexDistributionDataSeed()
-//        {
-//            Vector3 bloomPosition = GetBloomPosition();
-
-//            // (0) 最初のデータを追加します。
-
-//            while (true)
-//            {
-//                bloomPosition = GetBloomPosition();
-
-//                if (bloomPosition.x == Effect_FlowerTableGlobal.FlowerPositionInvalidValue.x)
-//                {
-//                    continue;
-//                }
-
-//                this.textureIndexDistributionDataList.Add(new TextureIndexDistributionData()
-//                {
-//                    textureIndex = this.flowerManager.GetRandomMainTextureIndex(),
-//                    position = bloomPosition
-//                });
-
-//                break;
-//            }
-
-//            while (true)
-//            {
-//                // (1) ランダムに咲く位置を取得します。
-
-//                bloomPosition = GetBloomPosition();
-
-//                if (bloomPosition.x == Effect_FlowerTableGlobal.FlowerPositionInvalidValue.x)
-//                {
-//                    continue;
-//                }
-
-//                // (2) 既存の分布データの中から近い距離にあるデータを探します。
-
-//                float length = 0;
-//                float minLength = Vector3.SqrMagnitude
-//                    (bloomPosition - this.textureIndexDistributionDataList[0].position);
-
-//                for (int i = 1; i < this.textureIndexDistributionDataList.Count; i++)
-//                {
-//                    length = Vector3.SqrMagnitude
-//                        (bloomPosition - this.textureIndexDistributionDataList[i].position);
-
-//                    if (length < minLength)
-//                    {
-//                        minLength = length;
-//                    }
-//                }
-
-//                // (3) ある程度距離が離れていたらシード値として採用します。
-
-//                if (this.nearDistributionThreshold < minLength)
-//                {
-//                    this.textureIndexDistributionDataList.Add(new TextureIndexDistributionData()
-//                    {
-//                        textureIndex = this.flowerManager.GetRandomMainTextureIndex(),
-//                        position = bloomPosition
-//                    });
-//                }
-
-//                // (4) ある程度シードが溜まったら終了します。
-
-//                if (this.distributionSeedCount <= this.textureIndexDistributionDataList.Count)
-//                {
-//                    break;
-//                }
-//            }
-//        }
-
-//        /// <summary>
-//        /// 花を咲かせます。
-//        /// </summary>
-//        protected override void BloomFlower()
-//        {
-//            // (0) 無効な位置に咲かそうとしていたら終了します。
-
-//            Vector3 bloomPosition = GetBloomPosition();
-
-//            if (bloomPosition.x == Effect_FlowerTableGlobal.FlowerPositionInvalidValue.x)
-//            {
-//                return;
-//            }
-
-//            int textureIndexDistributionDataListCount = this.textureIndexDistributionDataList.Count;
-//            int textureIndex;
-
-
-//            float minLength = Vector3.SqrMagnitude
-//                (bloomPosition - this.textureIndexDistributionDataList[0].position);
-
-//            int minLengthIndex = 0;
-
-//            // (1) 既存の分布データの中から最短距離にあるデータを取得します。
-
-//            for (int i = 1; i < textureIndexDistributionDataListCount; i++)
-//            {
-//                float length = Vector3.SqrMagnitude
-//                    (bloomPosition - this.textureIndexDistributionDataList[i].position);
-
-//                if (length < minLength)
-//                {
-//                    minLength = length;
-//                    minLengthIndex = i;
-//                }
-//            }
-
-//            // (2) 最短距離にあるデータが、一定の距離以下にあるかを検出します。
-
-//            // (2.1) 近い位置に咲いているときは、そのテクスチャインデックスを取得して、分布データを更新します。
-
-//            TextureIndexDistributionData minLengthDistributionData
-//                = this.textureIndexDistributionDataList[minLengthIndex];
-
-//            if (minLength < this.nearDistributionThreshold)
-//            {
-//                textureIndex = minLengthDistributionData.textureIndex;
-
-//                this.textureIndexDistributionDataList[minLengthIndex] = new TextureIndexDistributionData()
-//                {
-//                    textureIndex = textureIndex,
-//                    position = (minLengthDistributionData.position + bloomPosition) / 2,
-//                };
-//            }
-
-//            // (2.2) 近い位置に咲いていないときは、新たにテクスチャインデックスを取得して、分布データを追加します。
-
-//            else
-//            {
-//                textureIndex = this.flowerManager.GetRandomMainTextureIndex();
-
-//                this.textureIndexDistributionDataList.Add(new TextureIndexDistributionData()
-//                {
-//                    textureIndex = textureIndex,
-//                    position = bloomPosition
-//                });
-//            }
-
-//            // (3) 花を咲かせます。
-
-//            this.flowerManager.BloomFlower(bloomPosition, textureIndex);
-//        }
-
-//        #endregion Method
-//    }
-//}
